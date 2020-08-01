ba527b107dab19d06f2e69738962522e18c77fc3
Pedro Henrique Soares de Cam | Cappta <pedro.soares@cappta.com.br>
2020-08-01 15:27:51 -0300

    Commit da aplicação

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Geolocation, Geoposition } from '@ionic-native/geolocation/ngx';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,27 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  public lat: any;
+  public log: any;
+
+  constructor(public geolocation: Geolocation) { }
+
+  public Geoloca() {
+
+    this.geolocation.getCurrentPosition().then((resp) => {
+      this.lat = resp.coords.latitude
+      this.log = resp.coords.longitude
+     }).catch((error) => {
+       console.log('Error getting location', error);
+     });
+     
+     let watch = this.geolocation.watchPosition();
+     watch.subscribe((data) => {
+      // data can be a set of coordinates, or an error (if an error occurred).
+      // data.coords.latitude
+      // data.coords.longitude
+     });
+
+  }
 
 }
